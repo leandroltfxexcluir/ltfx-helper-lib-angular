7d221334cf52f1f27f38cfda2a7b96d4c3c76c37
leandroltfx <leandrobsb55@hotmail.com>
2023-11-12 11:28:12 -0300

    importando lib na aplicação para testes

--- projects/ltfx-helper-app/src/app/app.module.ts
+++ projects/ltfx-helper-app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { LtfxHelperModule } from 'projects/ltfx-helper/src/public-api';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,6 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+
+    LtfxHelperModule,
 
     AppRoutingModule
   ],
